d2aa24c65c1313ccbb4578b383a715f02bd82dc4
Alessandra Correa <bkpalecorrea@gmail.com>
2026-02-06 08:21:14 -0300

    proxima etapa route

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# ["chinese", "italian", "japanese", "french", "belgian"]
+
+
+
+["chinese", "italian", "japanese", "french", "belgian"].each do |category|
+  Restaurant.find_or_create_by!(category: category) do |restaurant|
+    restaurant.name = "#{category.capitalize} Restaurant"
+    restaurant.address = "#{category.capitalize} restaurant address."
+    restaurant.phone_number = "00000-0000"
+  end
+end
